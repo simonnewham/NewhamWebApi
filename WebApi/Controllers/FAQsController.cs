f597f7bb29ec911eae4c7b467a440e55cdb98cc2
Simon Newham <snewham09@gmail.com>
2023-05-13 13:45:17 +0200

    initial faq controller

---
+++ WebApi/Controllers/FAQsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+	[Route("FAQs")]
+	public class FAQsController
+	{
+		private readonly IFAQService _faqService;
+
+		public FAQsController(IFAQService faqService)
+		{
+			_faqService = faqService;
+		}
+
+		[HttpGet]
+		[Route("FAQList")]
+		public IEnumerable<FAQDetailsDto> GetFAQList()
+		{
+			return _faqService.GetFAQList();
+		} 
+    }
+}
